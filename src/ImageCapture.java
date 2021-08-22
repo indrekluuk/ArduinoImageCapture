b18402e496e1a4de8bb4e3dae6515747676dee3a
Indrek Luuk <indrek.luuk@gmail.com>
2021-08-22 23:41:35 +0300

    Added parity check and fix

--- src/ImageCapture.java
+++ src/ImageCapture.java
@@ -10,6 +10,7 @@ public class ImageCapture {
 
   enum PixelFormat {
     PIXEL_RGB565(2),
+    PIXEL_RGB565_WITH_PARITY_CHECK(2),
     PIXEL_GRAYSCALE(1);
 
     private int byteCount;
@@ -23,6 +24,21 @@ public class ImageCapture {
 
   private static final int MAX_W = 640;
   private static final int MAX_H = 480;
+
+
+  private static final int PIXEL_FORMAT_CODE_MASK = 0x07;
+  private static final int PIXEL_FORMAT_PARITY_CHECK_BIT = 0x80;
+
+  // Pixel byte parity check:
+  // Pixel Byte H: odd number of bits under H_BYTE_PARITY_CHECK and H_BYTE_PARITY_INVERT
+  // Pixel Byte L: even number of bits under L_BYTE_PARITY_CHECK and L_BYTE_PARITY_INVERT
+  //                                               H:RRRRRGGG
+  private static final int H_BYTE_PARITY_CHECK =  0b00100000;
+  private static final int H_BYTE_PARITY_INVERT = 0b00001000;
+  //                                               L:GGGBBBBB
+  private static final int L_BYTE_PARITY_CHECK =  0b00001000;
+  private static final int L_BYTE_PARITY_INVERT = 0b00100000;
+
 
   private static final byte START_COMMAND = (byte) 0x00;
   private static final byte COMMAND_NEW_FRAME = (byte) 0x01;
@@ -60,6 +76,11 @@ public class ImageCapture {
 
 
 
+  public void addReceivedBytes(byte [] receivedBytes) {
+    for (byte receivedByte : receivedBytes) {
+      addReceivedByte(receivedByte);
+    }
+  }
 
   public void addReceivedByte(byte receivedByte) {
     if (activeCommand < 0) {
@@ -124,6 +145,23 @@ public class ImageCapture {
     return d > max ? max : (d < min ? min : d);
   }
 
+
+  private PixelFormat getPixelFormat(int code) {
+    switch (code & PIXEL_FORMAT_CODE_MASK) {
+      default:
+        System.out.println("Unknown pixel format code '" + code + "'");
+      case 1:
+        if ((code & PIXEL_FORMAT_PARITY_CHECK_BIT) > 0) {
+          return PixelFormat.PIXEL_RGB565_WITH_PARITY_CHECK;
+        } else {
+          return PixelFormat.PIXEL_RGB565;
+        }
+      case 2:
+        return PixelFormat.PIXEL_GRAYSCALE;
+    }
+  }
+
+
   private void endOfLine() {
     imageCapturedCallback.imageCaptured(frame, frame.getCurrentLineIndex());
     frame.newLine();
@@ -133,39 +171,27 @@ public class ImageCapture {
   private void processPixelByte(byte receivedByte) {
     pixelBytes.write(receivedByte);
     if (pixelBytes.size() >= pixelFormat.getByteCount()) {
-      Pixel pixel = getPixel(pixelBytes.toByteArray());
-      frame.addPixel(pixel);
-      pixelBytes.reset();
-    }
-  }
-
-
-  private PixelFormat getPixelFormat(int code) {
-    switch (code & 0x07) {
-      default:
-        System.out.println("Unknown pixel format code '" + code + "'");
-      case 1:
-        return PixelFormat.PIXEL_RGB565;
-      case 2:
-        return PixelFormat.PIXEL_GRAYSCALE;
+      frame.addPixel(readAvailablePixel());
     }
   }
 
 
 
-  public Pixel getPixel(byte [] data) {
+  public Pixel readAvailablePixel() {
     switch (pixelFormat) {
       default:
       case PIXEL_RGB565: {
-        int rawPixelData = get2ByteInteger_H_L(data);
-        // rrrr rggg gggb bbbb
-        int r = (rawPixelData >> 8) & 0xF8;
-        int g = (rawPixelData >> 3) & 0xFC;
-        int b = (rawPixelData << 3) & 0xF8;
-        return new Pixel(r, g, b);
+        byte [] pixelDataBytes = pixelBytes.toByteArray();
+        pixelBytes.reset();
+        return parse2BytePixel(pixelDataBytes);
+      }
+      case PIXEL_RGB565_WITH_PARITY_CHECK: {
+        byte [] pixelDataBytes = readAvailablePixelDataBytesWithParityCheck();
+        return parse2BytePixel(pixelDataBytes);
       }
       case PIXEL_GRAYSCALE: {
-        int rawPixelData = data[0] & 0xFF;
+        int rawPixelData = pixelBytes.toByteArray()[0] & 0xFF;
+        pixelBytes.reset();
         int r = rawPixelData;
         int g = rawPixelData;
         int b = rawPixelData;
@@ -175,9 +201,52 @@ public class ImageCapture {
   }
 
 
+  private Pixel parse2BytePixel(byte [] pixelDataBytes) {
+    int rawPixelData = get2ByteInteger_H_L(pixelDataBytes);
+    // rrrr rggg gggb bbbb
+    int r = (rawPixelData >> 8) & 0xF8;
+    int g = (rawPixelData >> 3) & 0xFC;
+    int b = (rawPixelData << 3) & 0xF8;
+    return new Pixel(r, g, b);
+  }
 
 
+  private byte [] readAvailablePixelDataBytesWithParityCheck() {
+    byte [] pixedDataBytes = pixelBytes.toByteArray();
+    boolean isFirstByteHigh = isParityCheckHighByte(pixedDataBytes[0]);
+    boolean isSecondByteLow = isParityCheckLowByte(pixedDataBytes[1]);
 
+    if (isFirstByteHigh && isSecondByteLow) {
+      pixelBytes.reset();
+      return pixedDataBytes;
+
+    } else if (!isFirstByteHigh) {
+      byte [] fixedPixedDataBytes = new byte[2];
+      fixedPixedDataBytes[0] = 0;
+      fixedPixedDataBytes[1] = pixedDataBytes[0];
+      pixelBytes.reset();
+      pixelBytes.write(pixedDataBytes[1]);
+      return fixedPixedDataBytes;
+
+    } else {
+      byte [] fixedPixedDataBytes = new byte[2];
+      fixedPixedDataBytes[0] = pixedDataBytes[0];
+      fixedPixedDataBytes[1] = 0;
+      pixelBytes.reset();
+      pixelBytes.write(pixedDataBytes[1]);
+      return fixedPixedDataBytes;
+    }
+  }
+
+  private boolean isParityCheckHighByte(byte pixelByte) {
+    // Pixel Byte H: odd number of bits under H_BYTE_PARITY_CHECK and H_BYTE_PARITY_INVERT
+    return ((pixelByte & H_BYTE_PARITY_CHECK) > 0) != ((pixelByte & H_BYTE_PARITY_INVERT) > 0);
+  }
+
+  private boolean isParityCheckLowByte(byte pixelByte) {
+    // Pixel Byte L: even number of bits under L_BYTE_PARITY_CHECK and L_BYTE_PARITY_INVERT
+    return ((pixelByte & L_BYTE_PARITY_CHECK) > 0) == ((pixelByte & L_BYTE_PARITY_INVERT) > 0);
+  }
 
   private int get2ByteInteger_H_L(byte [] data) {
     if (data.length > 1) {
